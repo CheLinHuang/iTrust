bbd2afa445699b118a9de154e1d9f6f575b1aeb8
hip2 <hip2@illinois.edu>
2018-11-01 12:19:02 -0500

    Add bean test

---
+++ src/test/java/edu/ncsu/csc/itrust/unit/bean/OVRecordBeanTest.java
@@ -0,0 +1,13 @@
+package edu.ncsu.csc.itrust.unit.bean;
+
+import edu.ncsu.csc.itrust.model.old.beans.OVRecordBean;
+import junit.framework.TestCase;
+
+public class OVRecordBeanTest extends TestCase {
+
+    public void testBean() {
+        OVRecordBean ovrb = new OVRecordBean();
+
+        assertEquals();
+    }
+}
