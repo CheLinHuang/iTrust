9b726c018757acdaff05a4bc5a13b8dcf920dccf
dongjun2 <dongjun2@illinois.edu>
2018-11-08 04:43:55 -0600

    Unit Test: TravelHistoryBeanTest

--- src/test/java/edu/ncsu/csc/itrust/unit/bean/TravelHistoryBeanTest.java
+++ src/test/java/edu/ncsu/csc/itrust/unit/bean/TravelHistoryBeanTest.java
@@ -1,4 +1,53 @@
 package edu.ncsu.csc.itrust.unit.bean;
 
-public class TravelHistoryBeanTest {
+import edu.ncsu.csc.itrust.model.old.beans.TravelHistoryBean;
+import junit.framework.TestCase;
+
+public class TravelHistoryBeanTest extends TestCase {
+    TravelHistoryBean testBean;
+    TravelHistoryBean testBean2;
+    TravelHistoryBean testBean3;
+
+    /**
+     * setUp
+     */
+    @Override
+    public void setUp() {
+        testBean = new TravelHistoryBean();
+        testBean2 = new TravelHistoryBean();
+        testBean3 = new TravelHistoryBean();
+    }
+
+    /**
+     * testPatientMID
+     */
+    public void testPatientMID() {
+        testBean.setPatientMID(1L);
+        assertEquals(testBean.getPatientMID(), 1L);
+    }
+
+    /**
+     * testStartDate
+     */
+    public void testStartDate() {
+        testBean.setStartDate(null);
+
+        assertEquals(testBean.getStartDate(), null);
+    }
+
+    /**
+     * testEndDate
+     */
+    public void testEndDate() {
+        testBean.setEndDate(null);
+
+        assertEquals(testBean.getEndDate(), null);
+    }
+
+    /**
+     * testEquals
+     */
+    public void testEquals() {
+        assertTrue(testBean2.equals(testBean3));
+    }
 }
