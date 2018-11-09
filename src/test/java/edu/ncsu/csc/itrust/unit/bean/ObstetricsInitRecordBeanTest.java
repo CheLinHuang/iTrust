1c8e28e4abe9d750f7ba6de15484c9fdb2a2f9a1
Che-Lin Huang <clhuang2@illinois.edu>
2018-11-08 21:17:39 -0600

    ObstetricsInitRecord bean test update and add DAO tests

--- src/test/java/edu/ncsu/csc/itrust/unit/bean/ObstetricsInitRecordBeanTest.java
+++ src/test/java/edu/ncsu/csc/itrust/unit/bean/ObstetricsInitRecordBeanTest.java
@@ -3,22 +3,16 @@ package edu.ncsu.csc.itrust.unit.bean;
 import edu.ncsu.csc.itrust.model.old.beans.ObstetricsInitRecordBean;
 import junit.framework.TestCase;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.TimeZone;
-
 public class ObstetricsInitRecordBeanTest extends TestCase {
 
   public void testBean() {
     ObstetricsInitRecordBean orb = new ObstetricsInitRecordBean();
-    Calendar c = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
-    c.set(2018, Calendar.JANUARY, 1, 0, 0, 0);
-    orb.setLMP(c.getTime());
-    c.set(2018, Calendar.OCTOBER, 8, 0, 0, 0);
-    orb.setWeeksOfPregnant(new int[]{5, 2});
+    orb.setLMP("01/01/2018");
+    orb.setEDD("10/08/2018");
+    orb.setWeeksOfPregnant("14-6");
 
-    assertEquals(c.getTime(), orb.getEDD());
-    assertEquals(5, orb.getWeeksOfPregnant()[0]);
-    assertEquals(2, orb.getWeeksOfPregnant()[1]);
+    assertEquals("01/01/2018", orb.getLMP());
+    assertEquals("10/08/2018", orb.getEDD());
+    assertEquals("14-6", orb.getWeeksOfPregnant());
   }
 }
