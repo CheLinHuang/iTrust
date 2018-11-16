91183ebd0463f9ce7a6bc8378742275f9603fd7a
dongjun2 <dongjun2@illinois.edu>
2018-11-16 01:22:19 -0600

    Fixed DB population error

--- sql/data/TravelHistory1.sql
+++ sql/data/TravelHistory1.sql
@@ -1,5 +1,4 @@
 INSERT INTO TravelHistories(patientMID, startDate, endDate, travelledCities) VALUES
 (16, CONCAT(YEAR(NOW())-10, '-06-04 10:30:00'), CONCAT(YEAR(NOW())-5, '-06-04 10:30:00'), 'Vancouver,Canada, Cancun, Mexico'),
 (1, CONCAT(YEAR(NOW())-15, '-06-04 10:30:00'), CONCAT(YEAR(NOW())-10, '-06-04 10:30:00'), 'Paris,France'),
-(2, CONCAT(YEAR(NOW())-20, '-06-04 10:30:00'), CONCAT(YEAR(NOW())-19, '-06-04 10:30:00'), 'Barcelona,Spain')
-(3, CONCAT(YEAR(NOW())-18, '-06-04 10:30:00'), CONCAT(YEAR(NOW())-17, '-06-04 10:30:00'), 'Rome,Italy');
+(2, CONCAT(YEAR(NOW())-20, '-06-04 10:30:00'), CONCAT(YEAR(NOW())-19, '-06-04 10:30:00'), 'Barcelona,Spain');
