b099418cc92fd83be5bb40b15de4bd4e4ce7a274
dongjun2 <dongjun2@illinois.edu>
2018-11-16 01:27:35 -0600

    realistic travel data

--- sql/data/TravelHistory1.sql
+++ sql/data/TravelHistory1.sql
@@ -1,4 +1,4 @@
 INSERT INTO TravelHistories(patientMID, startDate, endDate, travelledCities) VALUES
-(16, CONCAT(YEAR(NOW())-10, '-06-04 10:30:00'), CONCAT(YEAR(NOW())-5, '-06-04 10:30:00'), 'Vancouver,Canada, Cancun, Mexico'),
-(1, CONCAT(YEAR(NOW())-15, '-06-04 10:30:00'), CONCAT(YEAR(NOW())-10, '-06-04 10:30:00'), 'Paris,France'),
-(2, CONCAT(YEAR(NOW())-20, '-06-04 10:30:00'), CONCAT(YEAR(NOW())-19, '-06-04 10:30:00'), 'Barcelona,Spain');
+(16, CONCAT(YEAR(NOW())-5, '-06-04 10:30:00'), CONCAT(YEAR(NOW())-5, '-06-10 10:30:00'), 'Vancouver,Canada, Cancun, Mexico'),
+(1, CONCAT(YEAR(NOW())-3, '-07-11 10:30:00'), CONCAT(YEAR(NOW())-3, '-07-24 10:30:00'), 'Paris,France Seoul,South Korea'),
+(2, CONCAT(YEAR(NOW())-1, '-01-21 10:30:00'), CONCAT(YEAR(NOW())-1, '-01-29 10:30:00'), 'Barcelona,Spain Tokyo,Japan');
