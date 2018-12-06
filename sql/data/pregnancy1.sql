026b2e218f8c8c2f208c7d84c7c45b8035beac66
Roger Ho <pingche2@illinois.edu>
2018-12-06 16:18:11 -0600

    Added more test data for UC-93 Che-Lin Huang(clhuang2) & Roger Ho (pingche2)

--- sql/data/pregnancy1.sql
+++ sql/data/pregnancy1.sql
@@ -6,9 +6,67 @@ hoursInLabor,
 weightGain,
 deliveryType)
 VALUES
-(6,
-2018,
-'40-6',
-4.5,
-20,
-'vaginal delivery');
+    (
+      6,
+      2018,
+      '40-6',
+      4.5,
+      20,
+      'vaginal delivery'
+    ),
+    (
+      6,
+      2017,
+      '35-4',
+      5.0,
+      25,
+      'vaginal delivery forceps assist'
+    ),
+    (
+      6,
+      2018,
+      '31-6',
+      5.5,
+      23.4,
+      'caesarean section'
+    ),
+    (
+      7,
+      2016,
+      '40-6',
+      4.5,
+      20,
+      'vaginal delivery'
+    ),
+    (
+      7,
+      2017,
+      '40-6',
+      4.5,
+      20,
+      'miscarriage'
+    ),
+    (
+      8,
+      2013,
+      '40-6',
+      4.5,
+      20,
+      'vaginal delivery'
+    ),
+    (
+      8,
+      2018,
+      '40-6',
+      4.5,
+      20,
+      'caesarean section'
+    ),
+    (
+      12,
+      2018,
+      '40-6',
+      4.5,
+      20,
+      'caesarean section'
+    );
